fccb0d89df72fde55fc0bb83bb815ed01a5404b2
Gunwoong-Heo <gunwoong1129@gmail.com>
2022-02-05 10:42:31 +0900

    Initial commit

---
+++ src/main/java/study/metacodingsecurity/MetaCodingSecurityApplication.java
@@ -0,0 +1,13 @@
+package study.metacodingsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetaCodingSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetaCodingSecurityApplication.class, args);
+	}
+
+}
